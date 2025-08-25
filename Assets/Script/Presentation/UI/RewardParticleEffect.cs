b536b3a8984e2d8c084138d72276aafa2d9ac530
JonasMK <oms2385@naver.com>
2025-08-25 14:38:34 +0900

    Reward 연출 완료

---
+++ Assets/Script/Presentation/UI/RewardParticleEffect.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Script.Presentation.UI
+{
+    public class RewardParticleEffect : MonoBehaviour
+    {
+        #region Serialize Field
+
+        [Header("이펙트 출력 딜레이")] [SerializeField]
+        private float normalEffectDelay;
+
+        [SerializeField] private float epicEffectDelay;
+
+        [Space(10)] [SerializeField] private ParticleSystem normalEffect;
+        [SerializeField] private ParticleSystem epicEffect;
+
+        [Header("레어도 색상")] [SerializeField] private Color commonColor = Color.white;
+        [SerializeField] private Color epicColor;
+        [SerializeField] private Color legendaryColor;
+        [SerializeField] private Color supremeColor;
+
+        [Header("Presenter")] [SerializeField] private GetGachaResultPresenter presenter;
+
+        #endregion
+
+        #region Methods
+
+        public void OnSceneEnter()
+        {
+            StartCoroutine(StartNormal());
+            if (presenter.rewardItem.rarity < 2)
+            {
+                var main = normalEffect.main;
+                main.startColor = commonColor;
+            }
+            else
+            {
+                var main = normalEffect.main;
+                var epicMain = epicEffect.main;
+                switch (presenter.rewardItem.rarity)
+                {
+                    case 2:
+                        main.startColor = epicColor;
+                        epicMain.startColor = epicColor;
+                        break;
+                    case 3:
+                        main.startColor = legendaryColor;
+                        epicMain.startColor = legendaryColor;
+                        break;
+                    case 4:
+                        main.startColor = supremeColor;
+                        epicMain.startColor = supremeColor;
+                        break;
+                }
+
+                StartCoroutine(StartEpic());
+            }
+
+            normalEffect.Clear();
+            epicEffect.Clear();
+            normalEffect.gameObject.SetActive(false);
+            epicEffect.gameObject.SetActive(false);
+        }
+
+        public void OnSceneExit()
+        {
+            normalEffect.Clear();
+            epicEffect.Clear();
+            normalEffect.gameObject.SetActive(false);
+            epicEffect.gameObject.SetActive(false);
+        }
+        
+        public IEnumerator StartNormal()
+        {
+            yield return new WaitForSeconds(normalEffectDelay);
+            normalEffect.gameObject.SetActive(true);
+            normalEffect.Play();
+        }
+
+        public IEnumerator StartEpic()
+        {
+            yield return new WaitForSeconds(epicEffectDelay);
+            epicEffect.gameObject.SetActive(true);
+            epicEffect.Play();
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Script/Presentation/UI/UILine.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Script.Presentation.UI
+{
+    public class UILine : MonoBehaviour
+    {
+        #region Serialize Fields
+
+        [SerializeField] private RectTransform start;
+
+        [SerializeField] private RectTransform target;
+
+        [SerializeField] private Image lineImage;
+
+        #endregion
+        
+        RectTransform _lineRect;
+        RectTransform _parentRect;
+
+
+        private void Awake()
+        {
+            _lineRect = lineImage.rectTransform;
+            _parentRect = _lineRect.parent as RectTransform;
+        }
+
+        void LateUpdate()
+        {
+            Vector2 aLocal = WorldToParentLocal(start.position);
+            Vector2 bLocal = WorldToParentLocal(target.position);
+
+            Vector2 center = (aLocal + bLocal) * 0.5f;
+            _lineRect.anchoredPosition = center;
+
+            Vector2 delta = bLocal - aLocal;
+            float length = delta.magnitude;
+            float angle = Mathf.Atan2(delta.y, delta.x) * Mathf.Rad2Deg;
+
+            // 사이즈/회전 적용 (가로 길이 = length, 세로 = thickness)
+            _lineRect.sizeDelta = new Vector2(length, _lineRect.sizeDelta.y);
+            _lineRect.localRotation = Quaternion.Euler(0f, 0f, angle);
+        }
+        
+        private Vector2 WorldToParentLocal(Vector3 worldPos)
+        {
+            Vector2 screen = RectTransformUtility.WorldToScreenPoint(null, worldPos);
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(_parentRect, screen, null, out var local);
+            return local;
+        }
+    }
+}
